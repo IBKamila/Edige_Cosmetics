becc7103c93339dd78e1b542a81bf0d48fe271d9
ddahi <wtkhmina@gmail.com>
2022-06-13 20:06:42 +0600

    third

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="container_home">
+      <div className="titulCard">
+        <img />
+        <h1>Care</h1>
+      </div>
+      <div>
+        <img />$
+      </div>
+      <div>
+        <img />$
+      </div>
+      <div>
+        <img />$
+      </div>
+    </div>
+  );
+};
+
+export default Home;
