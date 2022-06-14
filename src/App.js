787072622d9983488e6b28f4baed379ca1fa4c1d
ddahi <wtkhmina@gmail.com>
2022-06-14 21:57:59 +0600

    authlogic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./Context/AuthContext";
 import MainRoutes from "./MainRoutes";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <MainRoutes />
+        <AuthContextProvider>
+          <MainRoutes />
+        </AuthContextProvider>
       </BrowserRouter>
     </div>
   );
